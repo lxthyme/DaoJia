0b869db10d0684a8406d2c68c765d42431aac16d
lxthyme <lx314333@gmail.com>
2021-10-09 18:46:00 +0800

    feat: 添加脚本

---
+++ DaoJia.setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+gsubmodule_add='sh /Users/lxthyme/Desktop/Lucky/.config/Anything-Backup/shell/git.submodule.add.sh '
+
+$gsubmodule_add git@gitlab.bl.com:iOS_Foundation/BLAPIManagers.git BLAPIManagers
+$gsubmodule_add git@gitlab.bl.com:iOS_ViewComponents/BLCouponCenterModule.git BLCouponCenterModule
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/BLCouponFloatingView.git BLCouponFloatingView
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/BLDaoJia.git BLDaoJia
+$gsubmodule_add git@gitlab.bl.com:iOS_DataCenters/BLHomeDataSource.git BLHomeDataSource
+$gsubmodule_add git@gitlab.bl.com:iOS_ViewComponents/BLHomePageViewComponents.git BLHomePageViewComponents
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/BLMapModule.git BLMapModule
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/BLOrder.git BLOrder
+$gsubmodule_add git@gitlab.bl.com:iOS_ViewComponents/BLOrderConfirmBottomView.git BLOrderConfirmBottomView
+$gsubmodule_add git@gitlab.bl.com:iOS_Foundation/BLRawAPIManager.git BLRawAPIManager
+$gsubmodule_add git@gitlab.bl.com:iOS/BLiOSAppImages.git BLiOSAppImages
+$gsubmodule_add git@gitlab.bl.com:iOS/BaiLian.git BaiLian
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJActivityGoodsList.git DJActivityGoodsList
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJAddressManageModule.git DJAddressManageModule
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJClassifyList.git DJClassifyList
+$gsubmodule_add git@gitlab.bl.com:iOS_DataCenters/DJGlobalStoreManager.git DJGlobalStoreManager
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJHome.git DJHome
+$gsubmodule_add git@gitlab.bl.com:DaoJia/DJNavigationView.git DJNavigationView
+$gsubmodule_add git@gitlab.bl.com:iOS_ViewComponents/DJNewModuleHome.git DJNewModuleHome
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJOrderChooseDeliveryTimeView.git DJOrderChooseDeliveryTimeView
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJOrderConfirm.git DJOrderConfirm
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJOrderListView.git DJOrderListView
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJPaySuccessful.git DJPaySuccessful
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJProductDetailView.git DJProductDetailView
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJResourceJumpManager.git DJResourceJumpManager
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJScanBarcodeOrderConfirm.git DJScanBarcodeOrderConfirm
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJScanBarcodeView.git DJScanBarcodeView
+$gsubmodule_add git@gitlab.bl.com:iOS_DataCenters/DJSearchHistoryManger.git DJSearchHistoryManger
+$gsubmodule_add git@gitlab.bl.com:xuyunpeng/DJShoppingCartModule.git DJShoppingCartModule
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJStoreList.git DJStoreList
+$gsubmodule_add git@gitlab.bl.com:iOS_Business/DJiOSAppImages.git DJiOSAppImages
---
+++ switch.branch.sh
@@ -0,0 +1,82 @@
+#!/bin/bash -e
+
+ProjectRoot='/Users/lxthyme/Desktop/Lucky/BL-gitlab/DaoJia'
+
+# 更新 master 分支
+info_ALL=(
+  master
+  BLAPIManagers
+  BLCouponCenterModule
+  BLCouponFloatingView
+  BLDaoJia
+  BLHomeDataSource
+  BLHomePageViewComponents
+  BLMapModule
+  BLOrder
+  BLOrderConfirmBottomView
+  BLRawAPIManager
+  BLiOSAppImages
+  BaiLian
+  DJActivityGoodsList
+  DJAddressManageModule
+  DJClassifyList
+  DJGlobalStoreManager
+  DJHome
+  DJNavigationView
+  DJNewModuleHome
+  DJOrderChooseDeliveryTimeView
+  DJOrderConfirm
+  DJOrderListView
+  DJPaySuccessful
+  DJProductDetailView
+  DJResourceJumpManager
+  DJScanBarcodeOrderConfirm
+  DJScanBarcodeView
+  DJSearchHistoryManger
+  DJShoppingCartModule
+  DJStoreList
+  DJiOSAppImages
+)
+
+# 新首页
+info_NewModuleHome=(damon_dev
+DJNewModuleHome BLDaoJia DJHome)
+
+# BYT-78838-集字浏览任务
+info_BYT_78838=(damon/BYT-78838-集字浏览任务
+DJNewModuleHome BLDaoJia DJHome)
+
+# info=(${info_ALL[@]})
+info=(${info_NewModuleHome[@]})
+# info=(${info_BYT_78838[@]})
+branch=${info[0]}
+Components=(${info[@]:1})
+echo -e "\033[37m ########## branch ${branch} \033[0m"
+echo -e "\033[37m ########## Components \033[43:37m[${#Components[@]}]\033[0m${Components[@]}\n \033[0m"
+
+echo -e "\033[37m ########## $branch Begin ########## \033[0m"
+for comp in ${Components[@]}
+do
+echo -e "\033[33m -->checkout $branch from $comp \033[0m"
+cd $ProjectRoot/$comp
+git checkout $branch
+git pull origin $branch
+done
+
+echo -e "\033[37m ########## $branch END ########## \033[0m\n"
+
+# echo -n "是否执行 pod install?(Y | y)"
+read -n1 -p "是否执行 pod install?(Y | y)" needInstall
+case $needInstall in
+(Y | y)
+  echo -e "\033[37m ########## pod install \033[0m"
+  cd $ProjectRoot/BaiLian
+  pod install
+  ;;
+(N | n)
+  echo "Skip to execute pod install!"
+  ;;
+(*)
+  echo "Error choice!"
+  ;;
+esac
