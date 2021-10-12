e0116bb0455f8240bfeeb199dea4d96d34882367
lxthyme <lx314333@gmail.com>
2021-10-12 09:52:23 +0800

    feat: 脚本更新

--- switch.branch.sh
+++ switch.branch.sh
@@ -39,37 +39,47 @@ info_ALL=(
 )
 
 # 新首页
-info_NewModuleHome=(damon_dev
-DJNewModuleHome BLDaoJia DJHome DJiOSAppImages)
+info_NewModuleHome=(damon_dev master_1015
+DJNewModuleHome BLDaoJia DJHome DJiOSAppImages BLiOSAppImages)
 
 # BYT-78838-集字浏览任务
-info_BYT_78838=(damon/BYT-78838-集字浏览任务
-BaiLian DJNewModuleHome BLDaoJia DJHome
-## dependency
-#BLHomePageViewComponents BLAPIManagers
+info_BYT_78838=(damon/BYT-78838-集字浏览任务 master_1015
+DJNewModuleHome BLDaoJia DJHome
+# dependency
+# BLHomePageViewComponents BLAPIManagers
 )
 
 # info=(${info_ALL[@]})
 # info=(${info_NewModuleHome[@]})
 info=(${info_BYT_78838[@]})
 branch=${info[0]}
-Components=(${info[@]:1})
-echo -e "\033[37m ########## branch ${branch} \033[0m"
+devBranch=${info[1]}
+Components=(${info[@]:2})
+echo -e "\033[37m ########## branch: \033[43:37m$branch\033[0m devBranch: \033[43:37m$devBranch\033[0m \033[0m"
 echo -e "\033[37m ########## Components \033[43:37m[${#Components[@]}]\033[0m${Components[@]}\n \033[0m"
 
-echo -e "\033[37m ########## $branch Begin ########## \033[0m"
 for comp in ${Components[@]}
 do
-echo -e "\033[33m -->checkout $branch from $comp \033[0m"
+echo -e "\033[33m -->checkout  from $comp \033[0m"
 cd $ProjectRoot/$comp
+
 git checkout $branch
 git pull origin $branch
 git push origin $branch
+
+git checkout $devBranch
+git pull origin $devBranch
+git merge $branch
+git push origin $devBranch
+
+git checkout $branch
+git merge $devBranch
+git push origin $branch
+echo -e "\033[33m ########## $comp END ########## \033[0m\n"
 done
 
-echo -e "\n\033[37m ########## $branch END ########## \033[0m\n"
+echo -e "\033[37m ########## Congratulations, All done!!! ########## \033[0m\n"
 
-# echo -n "是否执行 pod install?(Y | y)"
 read -n1 -p "是否执行 pod install?(Y | y)" needInstall
 case $needInstall in
 (Y | y)
